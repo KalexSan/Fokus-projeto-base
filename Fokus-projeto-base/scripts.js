7ce6ac44ade6bd2c93c4d6e3348cd299559e1126
Alex Sander Leite de Almeida <66784006+KalexSan@users.noreply.github.com>
2025-02-07 17:02:41 -0300

    Inserido o display Time, feito no formato padrão minuto : segundos

--- Fokus-projeto-base/scripts.js
+++ Fokus-projeto-base/scripts.js
@@ -13,23 +13,26 @@ const music = new Audio('sons/luna-rise-part-one.mp3')
 const musicPlay = new Audio('sons/play.wav')
 const musicPause = new Audio('sons/pause.mp3')
 const musicBeep = new Audio('sons/beep.mp3')
+const alternarIcone = document.querySelector('.app__card-primary-butto-icon')
 music.loop = true
-let tempoDecorridoSegundo = 10
+let tempoDecorridoSegundo = 1500
 let intervaloId = null
-const timeFocus = 1500
-const timeShortBreak = 300
-const timeLongBreak = 900
+
+displayTempo()
 
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoSegundo = 1500
     alterarContexto('foco', 'foco.png')
     focoBt.classList.add('active')
 })
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoSegundo = 300
     alterarContexto('descanso-curto', 'descanso-curto.png')
     curtoBt.classList.add('active')
 })
 longoBt.addEventListener('click', () => {
+    tempoDecorridoSegundo = 900
     alterarContexto('descanso-longo', 'descanso-longo.png')
     longoBt.classList.add('active')
 })
@@ -51,12 +54,13 @@ const contagemRegressiva = () => {
         musicBeep.play()
     }
     tempoDecorridoSegundo -= 1
-    console.log(tempoDecorridoSegundo)
+    displayTempo()
 }
 
 startPauseBt.addEventListener('click', iniciarouPausar )
 
 function alterarContexto(contexto, imagem) {
+    displayTempo()
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `imagens/${imagem}`)
     console.log(`> Alterando o contexto para: ${contexto}`)
@@ -82,15 +86,12 @@ function alterarContexto(contexto, imagem) {
     if (contexto == 'foco') {
         titulo.innerHTML = `Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
-            console.log('foco')
     } if (contexto == 'descanso-curto') {
         titulo.innerHTML = `Que tal dar uma respirada?<br>
             <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-            console.log('descanso-curto')
     } if (contexto == 'descanso-longo') {
         titulo.innerHTML = `Hora de voltar à superfécie.<br>
             <strong class="app__title-strong">Faça uma pausa longa.</strong>`
-            console.log('descanso-longo')
     }
 
     button.forEach(function (contexto) {
@@ -102,18 +103,22 @@ function iniciarouPausar() {
     if (intervaloId) {
         pausar()
         musicPause.play()
-        comecarouPausaSpan.innerHTML = 'Começar'
+        //comecarouPausaSpan.innerHTML = 'Começar'
+        //alternarIcone.setAttribute('src', 'imagens/play_arrow.png')
         return
     } else {
         musicPlay.play()
         intervaloId = setInterval(contagemRegressiva, 1000)
         comecarouPausaSpan.innerHTML = 'Pausar'
+        alternarIcone.setAttribute('src', 'imagens/pause.png')
     }
 }
 
 function pausar() {
     clearInterval(intervaloId)
     intervaloId = null
+    comecarouPausaSpan.innerHTML = 'Começar'
+    alternarIcone.setAttribute('src', 'imagens/play_arrow.png')
     musicBeep.pause()
     musicBeep.currentTime = 0
 }
@@ -121,4 +126,13 @@ function pausar() {
 function zerar() {
     pausar()
     tempoDecorridoSegundo = 10
+}
+
+function displayTempo () {
+    const time = new Date(tempoDecorridoSegundo * 1000)
+
+    const minutos = time.getUTCMinutes().toString().padStart(2, '0')
+    const segundos = time.getUTCSeconds().toString().padStart(2, '0')
+
+    displayTimer.innerHTML = `${minutos}:${segundos}`
 }
